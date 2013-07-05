9fc02f07882d065ebc309e4d2e98d3c92b1e8823
Mark Gannaway <mark@ganners.co.uk>
2013-07-05 23:48:40 +0100

    Added template for Compiler class

---
+++ src/BrainFuck/Compiler.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Our compiler will execute our tokens
+ */
+
+namespace BrainFuck;
+
+class Compiler {
+
+	private $_tokens;
+
+	/**
+	 * Constructor, sends the tokens to the compiler
+	 * 
+	 * @param array $tokens
+	 */
+	public function __construct(array $tokens) {
+
+	}
+
+	/**
+	 * Executes the tokens, runs the code magic style
+	 */
+	public function compile() {
+
+	}
+
+}
