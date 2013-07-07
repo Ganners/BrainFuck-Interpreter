060a75aa99d12b5eddf5aa88cb6b798e7d730dac
Mark Gannaway <mark@ganners.co.uk>
2013-07-07 17:43:00 +0100

    Made our compiler tests pass

--- src/BrainFuck/Compiler.php
+++ src/BrainFuck/Compiler.php
@@ -6,9 +6,11 @@
 
 namespace BrainFuck;
 
+use BrainFuck\BrainFuck_Exception;
+
 class Compiler {
 
-	private $_tokens;
+	protected $_tokens;
 
 	/**
 	 * Constructor, sends the tokens to the compiler
@@ -16,13 +18,24 @@ class Compiler {
 	 * @param array $tokens
 	 */
 	public function __construct(array $tokens) {
-
+		$this->_tokens = $tokens;
 	}
 
 	/**
 	 * Executes the tokens, runs the code magic style
 	 */
 	public function compile() {
+
+		//Convention states for an array of length 30000
+		$tape = array_fill(0, 30000, 0);
+		$ptr = 0;
+
+		for($tokenPointer = 0; $tokenPointer < count($this->_tokens); ++$tokenPointer) {
+
+			//Pass all data on pointers and tokens to our token, they can handle it from here
+			$this->_tokens[$tokenPointer]->runToken($tape, $ptr, $this->_tokens, $tokenPointer);
+
+		}
 
 	}
 
