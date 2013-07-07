694b94f3175d56d5e99f4c28460d797830ad2596
Mark Gannaway <mark@ganners.co.uk>
2013-07-07 16:57:08 +0100

    Filled in string tokenizer test

--- tests/BrainFuck/String_TokenizerTest.php
+++ tests/BrainFuck/String_TokenizerTest.php
@@ -1,26 +1,43 @@
 <?php
 
 use BrainFuck\String_Tokenizer;
+use BrainFuck\Token\Mock_Token;
+
+require_once(__DIR__ . '/../Mocks/Mock_Token.php');
 
 class String_TokenizerTest extends PHPUnit_Framework_TestCase {
 
 	public function setUp() {
 		
-	}
-
-	public function testConstruct() {
+		// Our fixture for tokens
+		$this->Tokens = array(
+			'BrainFuck\Token\Mock_Token' => array('X')
+			);
 
 	}
 
 	public function testTokenize() {
 
+		// Test that a string of white space returns an empty array of tokens
+		$String_Tokenizer_WS = new String_Tokenizer("\r\n\t ", $this->Tokens);
+        $this->assertCount(
+        	0, $String_Tokenizer_WS->tokenize());
+		
+		// Test that with 3 valid tokens in the string, an array of 3 is returned
+		$String_Tokenizer_XXX = new String_Tokenizer('XXX', $this->Tokens);
+        $this->assertCount(
+        	3, $String_Tokenizer_XXX->tokenize());
+
 	}
 
-	public function testFetchMatchingToken() {
+    /**
+     * @expectedException BrainFuck\BrainFuck_Exception
+     */
+	public function testTokenizeException() {
 
-	}
-
-	public function testFetchTokens() {
+		//Test that invalid token characters throw an exception
+		$String_Tokenizer_OO = new String_Tokenizer('OO', $this->Tokens);
+		$String_Tokenizer_OO->tokenize();
 
 	}
 
