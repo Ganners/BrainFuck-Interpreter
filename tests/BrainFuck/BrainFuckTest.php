3255ecb6560e274339c1f746a1f60c15563e953d
Mark Gannaway <mark@ganners.co.uk>
2013-07-07 15:08:39 +0100

    Added tests for main BrainFuck class

--- tests/BrainFuck/BrainFuckTest.php
+++ tests/BrainFuck/BrainFuckTest.php
@@ -10,23 +10,48 @@ class BrainFuckTest extends PHPUnit_Framework_TestCase {
 
 	public function testConstruct() {
 
+		// Test when we input some code with inputFile false
+		// it sets the _codeInput object variable
+		$BrainFuck_DirectInput = new BrainFuck('>+.', FALSE);
+    	$this->assertEquals(
+    		$this->readAttribute($BrainFuck_DirectInput, '_codeInput'), '>+.');
+		
+		// Test when we input a file, the contents of that file
+		// is sent to the _codeInput object variable
+		$BrainFuck_DirectInput = new BrainFuck(__DIR__ . '/../Test_Files/hello_world.bf', TRUE);
+    	$this->assertEquals(
+    		$this->readAttribute($BrainFuck_DirectInput, '_codeInput'),
+    		'++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.');
+		
 	}
 
-	public function testExecute() {
-
+    /**
+     * @expectedException BrainFuck\BrainFuck_Exception
+     */
+	public function testConstructTypeException() {
+		
+		// Test when $inputFile is not bool an \Exception is thrown
+		$BrainFuck_DirectInput = new BrainFuck('>+.', 'CAT');
+		
 	}
 
 	public function testReadFile() {
 
+		// Tests that a file can be read - Use reflection to make protected
+		// method available
+		$BrainFuck = new BrainFuck('>+.', FALSE);
+        $method = new ReflectionMethod(
+          $BrainFuck, '_readFile'
+        );
+
+        $method->setAccessible(TRUE);
+
+        //Test input of (int) 100
+        $this->assertEquals( 
+        	$method->invokeArgs( 
+        		$BrainFuck, array(__DIR__ . '/../Test_Files/hello_world.bf')), 
+        		'++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.');
+
 	}
-
-	public function testTokenizeInput() {
-
-	}
-
-	public function testCompileTokens() {
-
-	}
-
 
 }
