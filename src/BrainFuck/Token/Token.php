a6a11ca8fef85e7ea2e1759b9a3d79bb7a4ab60e
Mark Gannaway <mark@ganners.co.uk>
2013-07-07 17:44:38 +0100

    Modified our Token Abstract class to contain more base information

--- src/BrainFuck/Token/Token.php
+++ src/BrainFuck/Token/Token.php
@@ -2,6 +2,8 @@
 
 namespace BrainFuck\Token;
 
+use BrainFuck\BrainFuck_Exception;
+
 /**
  * Our base class for a Token, all
  * tokens should inherit from this.
@@ -20,7 +22,8 @@ abstract class Token {
 	 * @param int $column - The column the token was declared on
 	 */
 	public function __construct($line, $column) {
-
+		$this->_line = $line;
+		$this->_column = $column;
 	}
 
 	/**
@@ -29,7 +32,7 @@ abstract class Token {
 	 * @return int
 	 */
 	public function getLine() {
-
+		return $this->_line;
 	}
 
 	/**
@@ -38,6 +41,18 @@ abstract class Token {
 	 * @return int
 	 */
 	public function getColumn() {
+		return $this->_column;
+	}
+
+	/**
+	 * To be implemented by child classes
+	 * 
+	 * @param array $tape         - Our tape (array of 30000 containing ints)
+	 * @param int   $ptr          - Our current position on the tape
+	 * @param array $tokens       - Our array of tokens
+	 * @param int   $tokenPointer - Our current position on the tokens
+	 */
+	public function runToken(&$tape, &$ptr, &$tokens, &$tokenPointer) {
 
 	}
 
@@ -47,6 +62,7 @@ abstract class Token {
  * > - Increment the pointer to point to the next cell to the right.
  */
 class Increment_Pointer extends Token {
+	static $identifier = array('>');
 
 }
 
@@ -54,6 +70,7 @@ class Increment_Pointer extends Token {
  * < - Decrement the pointer to point to the next cell to the left.
  */
 class Decrement_Pointer extends Token {
+	static $identifier = array('<');
 
 }
 
@@ -61,6 +78,7 @@ class Decrement_Pointer extends Token {
  * + - Increment the byte pointed to by the pointer.
  */
 class Increment_Byte_Pointed_To extends Token {
+	static $identifier = array('+');
 
 }
 
@@ -68,6 +86,7 @@ class Increment_Byte_Pointed_To extends Token {
  * - - Decrement the byte pointed to by the pointer.
  */
 class Decrement_Byte_Pointed_To extends Token {
+	static $identifier = array('-');
 
 }
 
@@ -75,6 +94,7 @@ class Decrement_Byte_Pointed_To extends Token {
  * [ - Jump forward to the command after the corresponding ] if the byte at the pointer is zero.
  */
 class Jump_Forward extends Token {
+	static $identifier = array('[');
 
 }
 
@@ -82,6 +102,7 @@ class Jump_Forward extends Token {
  * ] - Jump back to the command after the corresponding [ if the byte at the pointer is non-zero.
  */
 class Jump_Backward extends Token {
+	static $identifier = array(']');
 
 }
 
@@ -89,6 +110,7 @@ class Jump_Backward extends Token {
  * . - Output the value of the byte at the pointer.
  */
 class Output_Value_At_Pointer extends Token {
+	static $identifier = array('.');
 
 }
 
@@ -96,5 +118,6 @@ class Output_Value_At_Pointer extends Token {
  * , - Accept one byte of input, storing its value in the byte at the pointer.
  */
 class Accept_Input extends Token {
+	static $identifier = array(',');
 
 }
